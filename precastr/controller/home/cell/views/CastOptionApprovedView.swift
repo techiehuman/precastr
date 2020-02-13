ae4c91e42fca22f8de0e064565f6e06f8d07099e
Mandeep Singh <techiesoulsin@gmail.com>
2020-02-13 16:19:43 +0800

    added new views

--- precastr/controller/home/cell/views/CastOptionApprovedView.swift
+++ precastr/controller/home/cell/views/CastOptionApprovedView.swift
@@ -1,15 +1,15 @@
 //
-//  CastOptionsView.swift
+//  CastOptionApprovedView.swift
 //  precastr
 //
-//  Created by Cenes_Dev on 12/02/2020.
+//  Created by Cenes_Dev on 13/02/2020.
 //  Copyright © 2020 Macbook. All rights reserved.
 //
 
 import UIKit
 
-class CastOptionsView: UIView {
-    
+class CastOptionApprovedView: UIView {
+
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
